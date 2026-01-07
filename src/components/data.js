d36823923dc70c1dd52bff22022f9e2e9bd1da33
Abhishek Mishra <iabm3568@gmail.com>
2026-01-07 18:10:56 +0530

    Create data.js

---
+++ src/components/data.js
@@ -0,0 +1,58 @@
+export let products = [
+  {
+    id: 1,
+    title: "Samsung Galaxy S8 64GB Black",
+    description: "Samsung Galaxy S23 Ultra 5G price in India starts from ₹1,24,999.",
+    image: "https://hotline.ua/img/tx/144/144618870_s265.jpg",
+    price: 16303
+  },
+  {
+    id: 2,
+    title: "Samsung Galaxy S9 64GB Black",
+    description: "High-end Android smartphone",
+    image: "",
+    price: 20888
+  },
+  {
+    id: 3,
+    title: "Samsung Galaxy S8+ 64GB Black",
+    description: "Premium Samsung phone",
+    image: "",
+    price: 18701
+  },
+  {
+    id: 4,
+    title: "Samsung Galaxy S9+ 64GB Black",
+    description: "Top tier phone",
+    image: "",
+    price: 49999
+  },
+  {
+    id: 5,
+    title: "Samsung Galaxy Note 9 6/128GB Midnight Black",
+    description: "Note series flagship",
+    image: "",
+    price: 29768
+  },
+  {
+    id: 6,
+    title: "Samsung Galaxy Note 8 64GB Black",
+    description: "Stylus phone",
+    image: "",
+    price: 22171
+  },
+  {
+    id: 7,
+    title: "Samsung Galaxy J6 2018 2/32GB Black",
+    description: "Budget phone",
+    image: "",
+    price: 5249
+  },
+  {
+    id: 8,
+    title: "Samsung Galaxy S6 Edge Black",
+    description: "Curved display phone",
+    image: "",
+    price: 12000
+  }
+];
